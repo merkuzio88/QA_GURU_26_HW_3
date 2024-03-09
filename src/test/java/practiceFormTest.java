2e2f0b73832a1fcc8ce57549f3e005479e60b890
Pavel Kulikov <pkulikov@secureducation.ru>
2024-03-10 01:17:14 +0700

    rename test class

--- src/test/java/practiceFormTest.java
+++ src/test/java/practiceFormTest.java
@@ -6,7 +6,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class tests {
+public class practiceFormTest {
 
     @BeforeAll
     static void beforeAll() {
